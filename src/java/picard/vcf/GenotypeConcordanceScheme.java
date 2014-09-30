6901b5a9e172f08c932773b0929abb59181efb00
Nils Homer <nhomer@broadinstitute.org>
2014-09-30 16:26:13 -0400

    updating default scheme: squash me

--- src/java/picard/vcf/GenotypeConcordanceScheme.java
+++ src/java/picard/vcf/GenotypeConcordanceScheme.java
@@ -25,8 +25,13 @@ public class GenotypeConcordanceScheme {
     protected static final Set<ContingencyState> FN_ONLY  = new HashSet<ContingencyState>() {{ add(ContingencyState.FN); }};
     protected static final Set<ContingencyState> TP_FN    = new HashSet<ContingencyState>() {{ add(ContingencyState.TP); add(ContingencyState.FN); }};
     protected static final Set<ContingencyState> TP_FP    = new HashSet<ContingencyState>() {{ add(ContingencyState.TP); add(ContingencyState.FP); }};
+    protected static final Set<ContingencyState> TP_TN    = new HashSet<ContingencyState>() {{ add(ContingencyState.TP); add(ContingencyState.TN); }};
     protected static final Set<ContingencyState> FP_FN    = new HashSet<ContingencyState>() {{ add(ContingencyState.FP); add(ContingencyState.FN); }};
+    protected static final Set<ContingencyState> FP_TN    = new HashSet<ContingencyState>() {{ add(ContingencyState.FP); add(ContingencyState.TN); }};
+    protected static final Set<ContingencyState> FP_TN_FN = new HashSet<ContingencyState>() {{ add(ContingencyState.FP); add(ContingencyState.TN); add(ContingencyState.FN); }};
     protected static final Set<ContingencyState> TP_FP_FN = new HashSet<ContingencyState>() {{ add(ContingencyState.TP); add(ContingencyState.FP); add(ContingencyState.FN);}};
+    protected static final Set<ContingencyState> TN_FN    = new HashSet<ContingencyState>() {{ add(ContingencyState.TN); add(ContingencyState.FN);}};
+
 
     /** Has this boolean been previously validated */
     private boolean isValidated = false;
@@ -48,20 +53,32 @@ public class GenotypeConcordanceScheme {
 
     /**
      * The scheme is defined in the constructor.
+     *
+     * The default scheme is derived from the GA4GH Benchmarking Work Group's proposed evaluation scheme.
+     *
+     * In general, we are comparing two sets of alleles.  Therefore, we can have zero or more contingency table values represented in one comparison.  For example, if the truthset is
+     * a heterozygous call with both alleles non-reference (HET_VAR1_VAR2), and the callset is a heterozygous call with both alleles non-reference with one of the alternate alleles
+     * matching an alternate allele in the callset, we would have a true positive, false positive, and false negative.  The true positive is from the matching alternate alleles, the
+     * false positive is the alternate allele found in the callset but not found in the truthset, and the false negative is the alternate in the truthset not found in the callset.
+     *
+     * We also include a true negative in cases where the reference allele is found in both the truthset and callset.
+     *
+     * We have no HET_VAR2_VAR3 case, as VAR2/VAR3 are simply symbolic, and so we can change HET_VAR2_VAR3 into the HET_VAR3_VAR4 case.
+     *
+     * Finally, we have NA cases, which represent tuples that our code can and should not reach.
      */
     public GenotypeConcordanceScheme() {
-
-        /**          ROW STATE            HOM_REF       HET_REF_VAR1       HET_VAR1_VAR2        HOM_VAR1        NO_CALL        LOW_GQ        LOW_DP        IS_MIXED    **/
-        addRow(CallState.HOM_REF,         TN_ONLY,      FN_ONLY,           FN_ONLY,             FN_ONLY,        EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
-        addRow(CallState.HET_REF_VAR1,    FP_ONLY,      TP_ONLY,           TP_FN,               TP_FN,          EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
-        addRow(CallState.HET_REF_VAR2,    NA,           TP_FP_FN,          TP_FN,               FN_ONLY,        EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
-        addRow(CallState.HET_REF_VAR3,    NA,           NA,                FP_FN,               NA,             EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
+        /**          ROW STATE            HOM_REF       HET_REF_VAR1       HET_VAR1_VAR2        HOM_VAR1        NO_CALL        LOW_GQ        LOW_DP        FILTERED      IS_MIXED    **/
+        addRow(CallState.HOM_REF,         TN_ONLY,      TN_FN,             FN_ONLY,             FN_ONLY,        EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
+        addRow(CallState.HET_REF_VAR1,    FP_TN,        TP_TN,             TP_FN,               TP_FN,          EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
+        addRow(CallState.HET_REF_VAR2,    NA,           FP_TN_FN,          NA,                  FP_FN,          NA,            NA,           NA,           NA,           NA);
+        addRow(CallState.HET_REF_VAR3,    NA,           NA,                FP_FN,               NA,             NA,            NA,           NA,           NA,           NA);
         addRow(CallState.HET_VAR1_VAR2,   FP_ONLY,      TP_FP,             TP_ONLY,             TP_FP_FN,       EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
-        addRow(CallState.HET_VAR1_VAR3,   NA,           NA,                TP_FP_FN,            NA,             EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
-        addRow(CallState.HET_VAR3_VAR4,   FP_ONLY,      FP_FN,             FP_FN,               FP_FN,          EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
+        addRow(CallState.HET_VAR1_VAR3,   NA,           NA,                TP_FP_FN,            NA,             NA,            NA,           NA,           NA,           NA);
+        addRow(CallState.HET_VAR3_VAR4,   FP_ONLY,      FP_FN,             FP_FN,               FP_FN,          NA,            NA,           NA,           NA,           NA);
         addRow(CallState.HOM_VAR1,        FP_ONLY,      TP_FP,             TP_FN,               TP_ONLY,        EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
-        addRow(CallState.HOM_VAR2,        NA,           FP_FN,             TP_FN,               FP_FN,          EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
-        addRow(CallState.HOM_VAR3,        NA,           NA,                FP_FN,               NA,             EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
+        addRow(CallState.HOM_VAR2,        NA,           FP_FN,             TP_FN,               FP_FN,          NA,            NA,           NA,           NA,           NA);
+        addRow(CallState.HOM_VAR3,        NA,           NA,                FP_FN,               NA,             NA,            NA,           NA,           NA,           NA);
         addRow(CallState.NO_CALL,         EMPTY,        EMPTY,             EMPTY,               EMPTY,          EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
         addRow(CallState.FILTERED,        EMPTY,        EMPTY,             EMPTY,               EMPTY,          EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
         addRow(CallState.LOW_GQ,          EMPTY,        EMPTY,             EMPTY,               EMPTY,          EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
@@ -69,7 +86,7 @@ public class GenotypeConcordanceScheme {
         addRow(CallState.IS_MIXED,        EMPTY,        EMPTY,             EMPTY,               EMPTY,          EMPTY,         EMPTY,        EMPTY,        EMPTY,        EMPTY);
 
         validateScheme();
-    }{}
+    }
 
     /**
      * Get the concordance state set associate with the given truth state and call state tuple.
